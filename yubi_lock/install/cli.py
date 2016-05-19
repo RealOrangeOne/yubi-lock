595f2d0d1a6741dbd7fc545bceff7f433bab8439
Jake Howard <git@theorangeone.net>
2016-05-19 19:56:15 +0100

    fix install

--- yubi_lock/install/cli.py
+++ yubi_lock/install/cli.py
@@ -1,15 +1,12 @@
 import click
-from sh import sudo
 import os
-
-
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+from yubi_lock.api import BASE_DIR
 
 
 @click.command('install', short_help='Install required files for insertion detection')
-@click.option('-v', '--verbose')
-def cli(verbose):
-    if verbose:
-        print("Exporting UDEV rule...")
-    sudo.cp(BASE_DIR + "../data/85-yubikey-screen-lock.rules", "/etc/udev/rules.d/85-yubikey-screen-lock.rules")
-    print('Export complete!')
+def cli():
+    exit_code = os.system("sudo cp {} {}".format(BASE_DIR + "../data/85-yubikey-screen-lock.rules", "/etc/udev/rules.d/85-yubikey-screen-lock.rules"))
+    if exit_code == 0:
+        print('Export complete!')
+    else:
+        print('Something went wrong!')
